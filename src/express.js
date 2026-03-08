0cef4402877f004717f62263eb76efd700c4e2db
suhasubasi <subasisuha@gmail.com>
2026-03-08 17:25:45 +0100

    add helmet and improve express security settings

--- src/express.js
+++ src/express.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import morgan from 'morgan'
+import helmet from 'helmet'
 import { router } from './routes/index.js'
 
 const app = express()
 
+app.disable('x-powered-by')
+
 app.use(morgan('dev'))
+app.use(helmet())
 app.use(express.json())
 
 app.use('/', router)
